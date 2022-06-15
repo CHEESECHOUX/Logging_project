ac7a9b1833ad09b5b69ba3ef87f4c20283a6b474
CHEESECHOUX <89918678+CHEESECHOUX@users.noreply.github.com>
2022-06-15 12:23:39 +0900

    Update send_message.py

--- AWS/send_message.py
+++ AWS/send_message.py
@@ -4,9 +4,9 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.client("sqs")
     
-    ip           = event['requestContext']['identity']['sourceIp']
+    ip = event['requestContext']['identity']['sourceIp']
     request_time = event['requestContext']['requestTime']
-    sqs_url      = "https://sqs.ap-northeast-2.amazonaws.com/703474273090/load-test"
+    sqs_url = "https://sqs.ap-northeast-2.amazonaws.com/703474273090/load-test"
     
     response = client.send_message(
         QueueUrl = sqs_url,
