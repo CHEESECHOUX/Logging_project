81d70f9733716262b67d971fc8bb6833ecf0d78e
CHEESECHOUX <89918678+CHEESECHOUX@users.noreply.github.com>
2022-06-15 12:22:30 +0900

    Feature: send messages from Lambda to SQS

---
+++ AWS/send_message.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    client = boto3.client("sqs")
+    
+    ip           = event['requestContext']['identity']['sourceIp']
+    request_time = event['requestContext']['requestTime']
+    sqs_url      = "https://sqs.ap-northeast-2.amazonaws.com/703474273090/load-test"
+    
+    response = client.send_message(
+        QueueUrl = sqs_url,
+        MessageBody = json.dumps((ip, request_time))
+    )
+            
+    return {
+        'statusCode': response["ResponseMetadata"]["HTTPStatusCode"],
+        'body': json.dumps(response['ResponseMetadata']['HTTPStatusCode'])
+    }
